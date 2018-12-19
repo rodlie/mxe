43d06d0ba134900cf3d675d7751d65e87c25b7cd
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2018-12-19 06:45:41 +0100

    update qt/im

--- src/imagemagick.mk
+++ src/imagemagick.mk
@@ -4,13 +4,13 @@ PKG             := imagemagick
 $(PKG)_WEBSITE  := https://www.imagemagick.org/
 $(PKG)_DESCR    := ImageMagick
 $(PKG)_IGNORE   :=
-$(PKG)_VERSION  := 6.9.9-51
-$(PKG)_CHECKSUM := aa5f6b1e97bd98fbf642c47487531bea0faf675c728d01130b52d2b46849104a
+$(PKG)_VERSION  := 6.9.10-17
+$(PKG)_CHECKSUM := 5de296c9588c3ab36cf8f488aab5a5a95d56a3d74f77721d9a42debacc6f5370
 $(PKG)_SUBDIR   := ImageMagick-$($(PKG)_VERSION)
 $(PKG)_FILE     := ImageMagick-$($(PKG)_VERSION).tar.xz
 $(PKG)_URL      := https://www.imagemagick.org/download/releases/$($(PKG)_FILE)
 $(PKG)_URL_2    := https://ftp.sunet.se/pub/multimedia/graphics/ImageMagick/$($(PKG)_FILE)
-$(PKG)_DEPS     := gcc xz bzip2 jpeg lcms libpng pthreads tiff zlib
+$(PKG)_DEPS     := gcc xz bzip2 jpeg lcms libpng tiff zlib
 
 define $(PKG)_UPDATE
     $(WGET) -q -O- 'https://www.imagemagick.org/' | \
@@ -21,11 +21,12 @@ endef
 define $(PKG)_BUILD
     cd '$(1)' && LDFLAGS="-lws2_32 -lz" ./configure \
         $(MXE_CONFIGURE_OPTS) \
-        --with-x=no --disable-docs --disable-modules \
-        --with-zlib --with-lzma --without-xz --without-jasper --enable-hdri --with-quantum-depth=32 \
+        --with-x=no --disable-docs --disable-modules --disable-openmp \
+        --with-zlib --with-lzma --without-jasper --enable-hdri --with-quantum-depth=16 \
         --enable-largefile --without-pango --without-webp --without-fftw --without-lqr \
-        --without-freetype --without-openexr --without-fontconfig
-    $(SED) -i 's/#define MAGICKCORE_HAVE_PTHREAD 1//g' '$(1)/magick/magick-baseconfig.h'
+        --without-freetype --without-openexr --without-fontconfig \
+        --enable-zero-configuration --with-package-release-name=Cyan
+    $(SED) -i 's/#define MAGICKCORE_HAVE_PTHREAD 0//g' '$(1)/magick/magick-baseconfig.h'
     $(SED) -i 's/#define MAGICKCORE_ZLIB_DELEGATE 1//g' '$(1)/magick/magick-config.h'
     $(MAKE) -C '$(1)' -j '$(JOBS)' bin_PROGRAMS=
     $(MAKE) -C '$(1)' -j 1 install bin_PROGRAMS=
@@ -33,5 +34,5 @@ define $(PKG)_BUILD
     '$(1)'/libtool --mode=link --tag=CXX \
         '$(TARGET)-g++' -Wall -Wextra -std=gnu++0x \
         '$(TEST_FILE)' -o '$(PREFIX)/$(TARGET)/bin/test-imagemagick.exe' \
-        `'$(TARGET)-pkg-config' ImageMagick++-6.Q32HDRI --cflags --libs`
+        `'$(TARGET)-pkg-config' ImageMagick++-6.Q16HDRI --cflags --libs`
 endef
