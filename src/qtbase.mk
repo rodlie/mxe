cda0edd81619ef91c1cc9f5e48cc9ddf2227d332
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2020-09-14 18:06:29 +0200

    fixes for cyan

--- src/qtbase.mk
+++ src/qtbase.mk
@@ -38,7 +38,7 @@ define $(PKG)_BUILD
             -force-pkg-config \
             -no-use-gold-linker \
             -release \
-            -shared \
+            -static \
             -sse2 -sse3 -ssse3 -sse4.1 -no-sse4.2 -no-avx -no-avx2 -no-avx512 \
             -prefix '$(PREFIX)/$(TARGET)/qt5' \
             -no-icu \
