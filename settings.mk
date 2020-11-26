6bea39a3c67575527f9e813b991f5e347441d7f4
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2020-11-27 00:01:58 +0100

    cyan stuff

--- settings.mk
+++ settings.mk
@@ -1,7 +1,7 @@
 #JOBS := 8
-MXE_TARGETS := i686-w64-mingw32.static x86_64-w64-mingw32.static
+MXE_TARGETS := x86_64-w64-mingw32.static
 MXE_PLUGIN_DIRS := plugins/gcc7 plugins/hevc
-LOCAL_PKG_LIST := cc imagemagick7 qtbase fontconfig
+LOCAL_PKG_LIST := cc cmake imagemagick7 qtbase fontconfig
 #LOCAL_PKG_LIST := cc cmake qtbase fontconfig lcms imagemagick7
 .DEFAULT_GOAL := local-pkg-list
 local-pkg-list: $(LOCAL_PKG_LIST)
