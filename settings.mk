8dd68d7b046917df5013840070e9169fe87ac24f
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2020-11-27 18:38:38 +0100

    minor

--- settings.mk
+++ settings.mk
@@ -1,7 +1,6 @@
 #JOBS := 8
-MXE_TARGETS := x86_64-w64-mingw32.static
+MXE_TARGETS := x86_64-w64-mingw32.static i686-w64-mingw32.static
 MXE_PLUGIN_DIRS := plugins/gcc7 plugins/hevc
-LOCAL_PKG_LIST := cc cmake imagemagick7 qtbase fontconfig
-#LOCAL_PKG_LIST := cc cmake qtbase fontconfig lcms imagemagick7
+LOCAL_PKG_LIST := cc cmake lcms imagemagick7 qtbase fontconfig
 .DEFAULT_GOAL := local-pkg-list
 local-pkg-list: $(LOCAL_PKG_LIST)
