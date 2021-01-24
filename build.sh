6510c12d10d9617e7dd4769b1ea215975d14cff5
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2021-01-24 18:39:15 +0100

    minor

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
-make clean || true
-rm -rf .ccache || true
-make MXE_USE_CCACHE= || echo "FAILED!!!"
+#!/bin/bash
+CLEAN=${CLEAN:-0}
 
+if [ "$CLEAN" = 1 ]; then
+    make clean || true
+    rm -rf .ccache || true
+fi
+
+make MXE_USE_CCACHE= || echo "FAILED!!!"
--- settings.mk
+++ settings.mk
@@ -1,5 +1,5 @@
 # JOBS := 8
-MXE_TARGETS := x86_64-w64-mingw32.static
+MXE_TARGETS := x86_64-w64-mingw32.static i686-w64-mingw32.static
 MXE_PLUGIN_DIRS := plugins/gcc7
 LOCAL_PKG_LIST := cc cmake qtbase
 .DEFAULT_GOAL := local-pkg-list
